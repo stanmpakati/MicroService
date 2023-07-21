7777119bb96922eb400efacc45c897ce93406d93
stanmpakati <stanmpakatig@gmail.com>
2023-07-21 13:36:12 +0200

    fixed not routing bug

--- authentication-service/src/main/kotlin/com/stancloud/authenticationservice/HealthController.kt
+++ authentication-service/src/main/kotlin/com/stancloud/authenticationservice/HealthController.kt
@@ -4,7 +4,7 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-@RequestMapping("/api/v1/health")
+@RequestMapping("/auth/health")
 class HealthController {
   @RequestMapping("/ping")
   fun ping(): String {
